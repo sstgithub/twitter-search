2dbf9a47ecefe09ebd34958db1fb2913fe593e2e
Sid Tantia <sstantia@gmail.com>
2017-02-01 23:19:10 -0800

    Set up caching

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,10 @@
 class SearchesController < ApplicationController
   def navbar_search
     twitter_api = TwitterApi.new
-    @tweets = twitter_api.find_recent_tweets_for(params[:q])
+    cache_key = ["timeline", params[:q]]
+    @tweets = Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
+      twitter_api.find_recent_tweets_for(params[:q])
+    end
     render 'navbar_search', layout: 'application'
   end
 end
